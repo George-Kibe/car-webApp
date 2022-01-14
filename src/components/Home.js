cd2e052439b255b09ea18424729a3d15d855ce07
= <=>
2022-01-14 14:02:41 +0300

    Header and sections added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,48 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section 
+                title="Model S"
+                description="Order online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"/>
+            <Section 
+                title="Model Y"
+                description="Order online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"/>
+            <Section 
+                title="Model 3"
+                description="Order online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"/>
+            <Section 
+                title="Model X"
+                description="Order online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"/>
+            <Section 
+                title="Lowest Cost Solar Panel"
+                description="Solar Panels"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"/>
+            <Section 
+                title="Model 3"
+                description="Solar Roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"/>
+            <Section 
+                title="Accessories"
+                description="Accessoris"
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop Now"
+                rightBtnText=""/>
         </Container>
     )
 }
