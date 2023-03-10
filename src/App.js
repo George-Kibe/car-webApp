4102be8ae6120da03d17be5e5ab4223fc70f4469
George-Kibe <georgekibewambui@gmail.com>
2023-03-10 11:20:40 +0300

    Deploying to github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App relative">
       <Header />
-      <Home />    
+      <Home />  
+      <Footer />  
     </div>
   );
 }
